0820997c0d2bd3975cee75c0d5332fe84a8e8bf6
vibudara <vibudara2507@gmail.com>
2026-03-24 22:12:15 +0530

    UC4: Display multiple names with proper formatting

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -2,11 +2,31 @@ public class HelloApp {
 
     public static void main(String[] args) {
 
-        // Get the name from command line argument
-        String name = args[0];
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        }
+        else if (args.length == 1) {
+            System.out.println("Hello, " + args[0] + "!");
+        }
+        else {
+            System.out.print("Hello, ");
 
-        // Display greeting
-        System.out.println("Hello, " + name + "!");
+            for (int i = 0; i < args.length; i++) {
 
+                if (i == args.length - 1) {
+                    System.out.print("and " + args[i]);
+                } else {
+                    System.out.print(args[i]);
+
+                    if (i < args.length - 2) {
+                        System.out.print(", ");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+            }
+
+            System.out.println("!");
+        }
     }
 }
