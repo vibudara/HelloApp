77a6d2567c2a37930f0c06aa9999e624655c0e1a
vibudara <vibudara2507@gmail.com>
2026-03-28 19:11:54 +0530

    UC6 implemented using substring method

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,19 +1,21 @@
 public class HelloApp {
-    public static void main(String[] args) {
-        String name = "World";
 
-        if (args.length > 0) {
-            StringBuilder nameBuilder = new StringBuilder();
-            boolean first = true;
-            for (String arg : args) {
-                if (!first) {
-                    nameBuilder.append(", ");
-                }
-                nameBuilder.append(arg);
-                first = false;
-            }
-            name = nameBuilder.toString();
+    public static void main(String[] args) {
+
+
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
         }
-        System.out.println("Hello, " + name + "!");
+
+        StringBuilder sb = new StringBuilder();
+
+        for (String name : args) {
+            sb.append(name).append(", ");
+        }
+
+        String result = sb.toString().substring(0, sb.length() - 2);
+
+        System.out.println("Hello, " + result + "!");
     }
 }
